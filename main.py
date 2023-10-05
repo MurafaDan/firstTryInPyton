eb32e31a9940922a9f3e706c6a09018b641f6d93
MurafaDan <murafadan@gmail.com>
2023-10-05 17:50:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import turtle
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+d=[1,2,3,4,4.5]
+
+print(d)
+print("234/6.3 este : " , 234/6.3)
+
+print(type(d))
+
+print(type(264/6.3))
+
+y = 2j+5
+print(y,type(y))
+
+z="paitan"
+print(z,type(z))
+
+
+
+ee="hello"+" there"
+print(ee)
+
+
+print("Hihi haha "*10)
+
+print(2**3/5);
+
+n= input("scrie pe n:  ")
+print(n)
+
+print(input("Scrie inca unu"  ))
+
+
+def try1 (a=2 , b=3 , c=5):
+ print(a*b*c)
+ print("functie")
+
+try1(a=2 , b=3  , c=5)
+
+
+
+def greet(lang):
+
+
+    input()
+    if lang=='es':
+        print('Hola')
+    elif lang == 'fr' :
+        print('Bounjour')
+    else:
+        print('alta limba')
+
+
+greet('fr')
+
+
+
+
